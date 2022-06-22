5b4d6b96aac2fe23b3ff851855ac2e71d85fca28
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-06-22 09:58:58 +0700

    Add testing unit

--- src/screens/DaftarJual/index.js
+++ src/screens/DaftarJual/index.js
@@ -1,69 +1,17 @@
-import {Text, StyleSheet, ScrollView} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 import React from 'react';
-import {IdentityCard, InputAdd, ItemNotificationCard} from '../../component';
-import {moderateScale} from 'react-native-size-matters';
 
 const DaftarJual = () => {
   return (
-    <ScrollView style={styles.page}>
+    <View style={styles.page}>
       <Text>DaftarJual</Text>
-      <IdentityCard
-        urlImage={
-          'https://assets.pikiran-rakyat.com/crop/12x16:705x712/x/photo/2021/08/18/2258085066.jpeg'
-        }
-        nama="Jaemin"
-        kota="Jakarta"
-        typeIdentity="Penjual"
-        onPressButton={() => null}
-      />
-      <IdentityCard
-        urlImage="https://assets.pikiran-rakyat.com/crop/12x16:705x712/x/photo/2021/08/18/2258085066.jpeg"
-        nama="Jaemin"
-        kota="Jakarta"
-        typeIdentity="Pembeli"
-      />
-      <InputAdd onPress={() => null} product={true} style={styles.input} />
-      <InputAdd onPress={() => null} />
-      <ItemNotificationCard
-        productName="Jaemin"
-        date="20 Apr, 14:04"
-        productPrice="Rp. 12728379479"
-        typeNotif="Berhasil diterbitkan"
-        urlImage="https://assets.pikiran-rakyat.com/crop/12x16:705x712/x/photo/2021/08/18/2258085066.jpeg"
-      />
-      <ItemNotificationCard
-        productName="Jaemin"
-        date="20 Apr, 14:04"
-        productPrice="Rp. 12728379479"
-        seen={true}
-        typeNotif="Penawaran Product"
-        tawaran="Rp. 150000"
-        urlImage="https://assets.pikiran-rakyat.com/crop/12x16:705x712/x/photo/2021/08/18/2258085066.jpeg"
-      />
-      <ItemNotificationCard
-        productName="Jaemin"
-        date="20 Apr, 14:04"
-        productPrice="Rp. 12728379479"
-        typeNotif="Penawaran Product"
-        tawaran="Rp. 150000"
-        seen={true}
-        button={true}
-        textButton1="Status"
-        textButton2="Hubungi"
-        status="Completed"
-        urlImage="https://assets.pikiran-rakyat.com/crop/12x16:705x712/x/photo/2021/08/18/2258085066.jpeg"
-      />
-    </ScrollView>
+    </View>
   );
 };
 
 export default DaftarJual;
 
 const styles = StyleSheet.create({
-  input: {
-    width: moderateScale(300),
-    height: moderateScale(500),
-  },
   page: {
     flex: 1,
   },
