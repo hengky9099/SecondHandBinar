51f9d4e2bd558d31da117da5343002b407534513
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-07-06 06:15:36 +0700

    return home back to the beginning

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,10 @@
 import {Text, View} from 'react-native';
 import React from 'react';
-import Button from '../../component/Button';
-import Input from '../../component/Input';
-import IdentityCard from '../../component/IdentityCard';
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
-      <Button textButton1="Hallo" />
-      <Input placeholder="Hallo" />
-      <IdentityCard nama="Nana" kota="Busan" />
     </View>
   );
 };
