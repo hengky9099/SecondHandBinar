e709bc47adb0c771f2785433b23b1a4f805693c6
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-07-06 10:53:43 +0700

    feat/fix: crashlytics firebase + eslint

--- src/screens/DaftarJual/index.js
+++ src/screens/DaftarJual/index.js
@@ -1,10 +1,41 @@
-import {Text, StyleSheet, View} from 'react-native';
-import React from 'react';
+import {Text, StyleSheet, View, Button} from 'react-native';
+import React, {useEffect} from 'react';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 const DaftarJual = () => {
+  async function onSignIn(user) {
+    crashlytics().log('User signed in.');
+    await Promise.all([
+      crashlytics().setUserId(user.uid),
+      crashlytics().setAttribute('credits', String(user.credits)),
+      crashlytics().setAttributes({
+        role: 'admin',
+        followers: '13',
+        email: user.email,
+        username: user.username,
+      }),
+    ]);
+  }
+
+  useEffect(() => {
+    crashlytics().log('App mounted.');
+  }, []);
+
   return (
     <View style={styles.page}>
       <Text>DaftarJual</Text>
+      <Button
+        title="Sign In"
+        onPress={() =>
+          onSignIn({
+            uid: 'Aa0Bb1Cc2Dd3Ee4Ff5Gg6Hh7Ii8Jj9',
+            username: 'Joaquin Phoenix',
+            email: 'phoenix@example.com',
+            credits: 42,
+          })
+        }
+      />
+      <Button title="Test Crash" onPress={() => crashlytics().crash()} />
     </View>
   );
 };
