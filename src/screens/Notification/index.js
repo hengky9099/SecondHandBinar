1b0cbfc94588d67779d25134cbc70ef115fc511f
Diah Novianti <diahnovianti0711@gmail.com>
2022-07-19 21:26:04 +0700

    fixing: styling and cleaning code

--- src/screens/Notification/index.js
+++ src/screens/Notification/index.js
@@ -1,23 +1,23 @@
 import {View, Alert, FlatList, RefreshControl} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {ItemNotificationCard, Poppins} from '../../component';
+import {ItemNotificationCard, Poppins, StatusBarCore} from '../../component';
 import {useDispatch, useSelector} from 'react-redux';
 import {setLoading} from '../../redux/globalAction';
 import axios from 'axios';
 import {baseUrl} from '@env';
-import {setNotification, setRefreshing} from './redux/action';
 import {navigate} from '../../helpers/navigate';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {currencyToIDR, thisDate} from '../../helpers/change';
 import {setLogin} from '../Login/redux/action';
 import {useCallback} from 'react';
 import styles from './styles';
+import {COLORS} from '../../helpers/colors';
 
 const Notification = () => {
   const dispatch = useDispatch();
   const {dataLogin} = useSelector(state => state.login);
-  const {refreshing} = useSelector(state => state.notifikasi);
   const [notifikasi, setnotifikasi] = useState([]);
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
     getDataNotification();
@@ -30,12 +30,10 @@ const Notification = () => {
       const res = await axios.get(`${baseUrl}/notification`, {
         headers: {access_token: `${dataLogin.access_token}`},
       });
-      setnotifikasi([...res.data]);
+      setnotifikasi(res.data);
       console.log('Data Notification: ', res.data);
-      dispatch(setNotification(res.data));
       if (res.status === 200) {
         dispatch(setLoading(false));
-        dispatch(setNotification(res.data));
       }
       if (res.status === 403) {
         setLogin();
@@ -67,14 +65,16 @@ const Notification = () => {
   }, [dataLogin, dispatch]);
 
   const onRefresh = () => {
-    dispatch(setRefreshing(true));
-    dispatch(getDataNotification(dataLogin));
+    setRefresh(true);
+    getDataNotification(dataLogin);
+    setRefresh(false);
   };
 
   const renderDataNotification = ({item}) => (
     <ItemNotificationCard
-      onPress={setRead(item.id)}
+      // onPress={setRead(item.id)}
       date={thisDate(item.transaction_date)}
+      urlImage={item.image_url}
       productName={item.product_name}
       productPrice={currencyToIDR(item.base_price)}
       tawaran={currencyToIDR(item.bid_price)}
@@ -82,31 +82,34 @@ const Notification = () => {
     />
   );
 
-  const setRead = id => {
-    for (let i in notifikasi) {
-      if (notifikasi[i].id === id) {
-        notifikasi[i].read = true;
-        break;
-      }
-    }
-    setNotification(notifikasi);
-  };
+  // const setRead = id => {
+  //   for (let i in notifikasi) {
+  //     if (notifikasi[i].id === id) {
+  //       notifikasi[i].read = true;
+  //       break;
+  //     }
+  //   }
+  //   setnotifikasi(notifikasi);
+  //   getDataNotification(id.read);
+  // };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Poppins style={styles.textHeader}>Notifikasi</Poppins>
+        <StatusBarCore backgroundColor={COLORS.white} barStyle="dark-content" />
         <View style={styles.containerNotifBar}>
           {notifikasi ? (
             <FlatList
               refreshControl={
-                <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+                <RefreshControl onRefresh={onRefresh} refreshing={refresh} />
               }
               data={notifikasi}
               renderItem={renderDataNotification}
               keyExtractor={(_item, index) => index}
               numColumns={1}
               key={1}
+              showsVerticalScrollIndicator={false}
               ListFooterComponent={<View style={styles.footerComponent} />}
             />
           ) : null}
